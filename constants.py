285d5e42406efd73a0363286d1d3e6e78e86dbcd
fidansamet <fidansamet@hacettepe.edu.tr>
2020-06-05 23:32:30 +0300

    clean

--- constants.py
+++
@@ -1,5 +0,0 @@
-DATASET_NAME = "MSASL"
-TRAIN_JSON_NAME = "MSASL_train.json"
-VAL_JSON_NAME = "MSASL_val.json"
-TEST_JSON_NAME = "MSASL_test.json"
-CLASSES = ["hello", "nice", "teacher", "eat", "no", "happy", "like", "orange", "want", "deaf"]
--- main.py
+++
@@ -1,4 +0,0 @@
-from download import download_dataset
-
-if __name__ == '__main__':
-    download_dataset()
